a1a3b32163f6f1e7cf2bc4fa68aad85f86658a37
Rafael de Macêdo <r4faelmf@gmail.com>
2024-05-02 15:09:35 -0300

    responsive fixes

--- wp-content/themes/mecao/archive-competition.php
+++ wp-content/themes/mecao/archive-competition.php
@@ -2,9 +2,9 @@
 get_header();
 ?>
 
-<main>
-	<div class="bg-white container mx-auto rounded-[24px] py-8 my-6 px-8 grid grid-cols-12 gap-8">
-		<div class="col-span-8 space-y-8">
+<main class="antialiased">
+	<div class="bg-white container mx-auto rounded-xl lg:rounded-[24px] p-4 lg:p-8 my-6 grid grid-cols-12 gap-4 lg:gap-8">
+		<div class="col-span-12 space-y-4 xl:col-span-8">
 			<?php
 
 			$currentCompetitions = new WP_Query([
@@ -41,15 +41,17 @@ get_header();
 			<?php }
 			wp_reset_postdata(); ?>
 		</div>
-		<div class="col-span-4">
+		<section class="col-span-12 xl:col-span-4 mt-8 lg:mt-0">
 
-			<?php get_template_part('template-parts/content', 'lastgame'); ?>
-			<?php get_template_part('template-parts/content', 'nextgame'); ?>
+			<div class="md:grid xl:block grid-cols-2 gap-4">
+				<?php get_template_part('template-parts/content', 'lastgame'); ?>
+				<?php get_template_part('template-parts/content', 'nextgame'); ?>
+			</div>
 
-			<div class="flex justify-center"><a href="#" class="inline-block text-sm text-stone-600 font-medium px-5 py-3 rounded-md border border-[#292524] border-opacity-10 shadow-small">Calendário de Jogos</a></div>
+			<div class="flex justify-center"><a href="<?php echo site_url('/jogos'); ?>" class="bg-white inline-block text-sm text-stone-600 font-medium px-5 py-3 rounded-md border border-gray-dark/20 hover:bg-stone-50 hover:border-gray-dark/40 transition-all duration-300 ease-in-out shadow-small">Calendário de Jogos</a></div>
 
 			<?php get_template_part('template-parts/content', 'standings'); ?>
-		</div>
+		</section>
 	</div>
 </main>
 
--- wp-content/themes/mecao/single-match.php
+++ wp-content/themes/mecao/single-match.php
@@ -4,97 +4,103 @@ get_header();
 ?>
 
 <main class="antialiased">
-	<div class="bg-white container mx-auto rounded-[24px] py-8 my-6 px-8 grid grid-cols-12 gap-8 w-full">
-		<?php while (have_posts()) {
-			the_post();
-		?>
-			<article class="pt-0 col-span-8 prose prose-xl prose-red w-full
+	<div class="bg-white container mx-auto rounded-xl lg:rounded-[24px] p-4 lg:p-8 my-6 grid grid-cols-12 gap-4 lg:gap-8">
+		<div class="col-span-12 space-y-4 lg:space-y-0 xl:col-span-8">
+			<?php while (have_posts()) {
+				the_post();
+			?>
+				<article class="prose sm:prose-xl prose-red w-full
 				prose-h2:uppercase prose-h2:text-red prose-h2:font-bold prose-h2:text-2xl
 				prose-h3:uppercase prose-h3:text-red-dark prose-h3:text-sm prose-h3:tracking-widest prose-h3:font-medium prose-h3:pt-[2em] prose-h3:mb-[1.25em]
-				prose-h4:uppercase prose-h4:text-stone-800 prose-h4:text-3xl prose-h4:font-bold prose-h4:tracking-wide prose-h4:leading-[1.25em] prose-h4:text-pretty
+				prose-h4:uppercase prose-h4:text-stone-800 prose-h4:font-bold prose-h4:tracking-wide prose-h4:text-pretty
+				prose-h4:text-lg prose-h4:leading-normal md:prose-h4:text-3xl md:prose-h4:leading-[1.25em]
 				prose-img:rounded-md
 				prose-p:leading-[1.6em]">
-				<?php
-				$matchTime = new DateTime(get_field('match_time'));
-				?>
-				<div class="flex items-center justify-between mb-4 px-1">
 					<?php
-					$formatter = new IntlDateFormatter(
-						'pt_BR',
-						IntlDateFormatter::SHORT,
-						IntlDateFormatter::SHORT,
-						'UTC',
-						IntlDateFormatter::GREGORIAN,
-					);
+					$matchTime = new DateTime(get_field('match_time'));
 					?>
-					<time class="flex items-center">
-						<div class="flex items-center space-x-4">
-							<div class="flex items-center space-x-1">
-								<span class=" font-semibold text-[2rem] text-blue leading-none">
-									<?php echo $matchTime->format('d'); ?>
-								</span>
-								<div class="flex-col space-y-0">
-									<span class="block uppercase leading-none text-stone-800 font-black text-sm">
-										<?php
-										$formatter->setPattern('cccc');
-										echo $formatter->format($matchTime);
-										?>
+					<div class="flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0 justify-between mb-4 px-1">
+						<?php
+						$formatter = new IntlDateFormatter(
+							'pt_BR',
+							IntlDateFormatter::SHORT,
+							IntlDateFormatter::SHORT,
+							'UTC',
+							IntlDateFormatter::GREGORIAN,
+						);
+						?>
+						<time class="flex items-center w-full md:w-auto">
+							<div class="flex items-center w-full space-x-4">
+								<div class="flex items-center space-x-2 w-full">
+									<span class=" font-semibold text-[2rem] text-blue leading-none">
+										<?php echo $matchTime->format('d'); ?>
 									</span>
-									<span class="block uppercase leading-none text-stone-800 text-sm">
+									<div class="flex-col space-y-0">
+										<span class="block uppercase leading-none text-stone-800 font-black text-sm">
+											<?php
+											$formatter->setPattern('cccc');
+											echo $formatter->format($matchTime);
+											?>
+										</span>
+										<span class="block uppercase leading-none text-stone-800 text-sm">
+											<?php
+											$formatter->setPattern('MMMM');
+											echo $formatter->format($matchTime);
+											?>
+										</span>
+									</div>
+								</div>
+								<div class="flex">
+									<span class="text-[2rem] leading-none">
 										<?php
-										$formatter->setPattern('MMMM');
+										$formatter->setPattern('HH:mm');
 										echo $formatter->format($matchTime);
 										?>
 									</span>
 								</div>
 							</div>
-							<div class="flex">
-								<span class="text-[2rem] leading-none">
-									<?php
-									$formatter->setPattern('HH:mm');
-									echo $formatter->format($matchTime);
-									?>
-								</span>
-							</div>
+						</time>
+						<div class="flex flex-col items-start md:self-center self-start space-y-0">
+							<span class="block uppercase leading-none text-stone-800 font-black text-sm">
+								<?php
+								echo get_the_title(get_field('match_competition')[0]);
+								?>
+							</span>
+							<span class="block uppercase leading-none text-stone-800 text-sm">
+								<?php
+								the_field('match_stadium');
+								?>
+							</span>
 						</div>
-					</time>
-					<div class="flex-col space-y-0">
-						<span class="block uppercase leading-none text-stone-800 font-black text-sm">
-							<?php
-							echo get_the_title(get_field('match_competition')[0]);
-							?>
-						</span>
-						<span class="block uppercase leading-none text-stone-800 text-sm">
-							<?php
-							the_field('match_stadium');
-							?>
-						</span>
 					</div>
-				</div>
-				<div class="grid grid-cols-match-score gap-x-8 gap-y-4 border-4 border-black/50 score-gradient p-8 w-full rounded-lg">
-					<?php
-					$homeTeam = get_field('home_team');
-					$awayTeam = get_field('away_team');
-					?>
-					<img class="h-14 w-auto justify-self-center m-0" src="<?php echo esc_url($homeTeam['url']) ?>" alt="">
-					<span class="text-yellow justify-self-stretch tracking-wider text-6xl uppercase font-display"><?php echo $homeTeam['title']; ?></span>
-					<span class="text-yellow tracking-wider text-right text-6xl uppercase font-display"><?php echo get_field('home_score') == '' ? '-' : get_field('home_score'); ?></span>
-					<img class="h-14 w-auto justify-self-center m-0" src="<?php echo esc_url($awayTeam['url']) ?>" alt="">
-					<span class="text-yellow justify-self-stretch tracking-wider text-6xl uppercase font-display"><?php echo $awayTeam['title']; ?></span>
-					<span class="text-yellow tracking-wider text-right text-6xl uppercase font-display"><?php echo get_field('away_score') == '' ? '-' : get_field('away_score'); ?></span>
-				</div>
-				<?php the_content(); ?>
-			</article>
-			<section class="col-span-4">
+					<div class="grid grid-cols-match-score gap-x-2 md:gap-x-8 gap-y-4 border-4 border-black/50 score-gradient p-2 md:p-8 w-full rounded-lg">
+						<?php
+						$homeTeam = get_field('home_team');
+						$awayTeam = get_field('away_team');
+						?>
+						<img class="h-14 w-auto justify-self-center m-0 sm:m-0" src="<?php echo esc_url($homeTeam['url']) ?>" alt="">
+						<span class="text-yellow justify-self-stretch self-center tracking-wider text-3xl md:text-6xl uppercase font-display"><?php echo $homeTeam['title']; ?></span>
+						<span class="text-yellow tracking-wider self-center text-right text-3xl md:text-6xl uppercase font-display"><?php echo get_field('home_score') == '' ? '-' : get_field('home_score'); ?></span>
+						<img class="h-14 w-auto justify-self-center m-0 sm:m-0" src="<?php echo esc_url($awayTeam['url']) ?>" alt="">
+						<span class="text-yellow justify-self-stretch self-center tracking-wider text-3xl md:text-6xl uppercase font-display"><?php echo $awayTeam['title']; ?></span>
+						<span class="text-yellow tracking-wider self-center text-right text-3xl md:text-6xl uppercase font-display"><?php echo get_field('away_score') == '' ? '-' : get_field('away_score'); ?></span>
+					</div>
+					<?php the_content(); ?>
+					<hr class="xl:hidden">
+				</article>
+			<?php } ?>
+		</div>
+		<section class="col-span-12 xl:col-span-4 mt-8 lg:mt-0">
 
+			<div class="md:grid xl:block grid-cols-2 gap-4">
 				<?php get_template_part('template-parts/content', 'lastgame'); ?>
 				<?php get_template_part('template-parts/content', 'nextgame'); ?>
+			</div>
 
-				<div class="flex justify-center"><a href="#" class="inline-block text-sm text-stone-600 font-medium px-5 py-3 rounded-md border border-[#292524] border-opacity-10 shadow-small">Calendário de Jogos</a></div>
+			<div class="flex justify-center"><a href="<?php echo site_url('/jogos'); ?>" class="bg-white inline-block text-sm text-stone-600 font-medium px-5 py-3 rounded-md border border-gray-dark/20 hover:bg-stone-50 hover:border-gray-dark/40 transition-all duration-300 ease-in-out shadow-small">Calendário de Jogos</a></div>
 
-				<?php get_template_part('template-parts/content', 'standings'); ?>
-			</section>
-		<?php } ?>
+			<?php get_template_part('template-parts/content', 'standings'); ?>
+		</section>
 	</div>
 </main>
 

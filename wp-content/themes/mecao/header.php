460e172c031445e9a1d9bba3fac55a733bc6d37f
Rafael de Macêdo <r4faelmf@gmail.com>
2024-05-02 17:54:03 -0300

    fix

--- wp-content/themes/mecao/header.php
+++ wp-content/themes/mecao/header.php
@@ -155,8 +155,8 @@
 							</a>
 							<button type="button" class="-m-2.5 rounded-md p-2.5 bg-white/20 text-white" @click="open = false">
 								<!-- <span class="sr-only">Fechar menu</span> -->
-								<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-									<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+								<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+									<path stroke-linecap=" round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 								</svg>
 							</button>
 						</div>

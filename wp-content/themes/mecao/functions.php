11a96a82702abf5a9941f1aac27ec6c5b49d401e
Rafael de Macêdo <r4faelmf@gmail.com>
2024-05-10 16:46:53 -0300

    add tag manager

--- wp-content/themes/mecao/functions.php
+++ wp-content/themes/mecao/functions.php
@@ -32,3 +32,36 @@ function mecao_remove_default_images($sizes)
 	return $sizes;
 }
 add_filter('intermediate_image_sizes_advanced', 'mecao_remove_default_images');
+
+function mecao_head_code()
+{ ?>
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-MP289NB7');
+	</script>
+	<!-- End Google Tag Manager -->
+<?
+}
+add_action('wp_head', 'mecao_head_code');
+
+function mecao_body_code()
+{ ?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MP289NB7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+<?php
+}
+add_action('wp_body_open', 'mecao_body_code');
